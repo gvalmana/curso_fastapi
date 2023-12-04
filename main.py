94365578aef74c0f2f6ca7718c0d3dd2254562ac
gvalmana <gustavo.shotokai@gmail.com>
2023-12-04 18:21:02 -0500

    commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 import uvicorn
 from typing import Optional
 
@@ -43,8 +43,17 @@ async def get_ser(id:int, edad:int, nombre:Optional[str] = None):
     return "User not found"
 
 @app.get('/welcome/{nombre}/{apellido}')
-async def welcome(nombre:str, apellido:str):
+async def welcome(nombre:str, apellido:str, response: Response):
     return f"Bienvenido {nombre} {apellido}"
+
+@app.get('/suma/{numero1}/{numero2}')
+async def sum(numero1: int, numero2:int, response: Response):
+    if numero1 + numero2 > 0:
+        response.status_code = status.HTTP_200_OK
+        return numero1 + numero2
+    else:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return None
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='127.0.0.1', port=3000, reload=True)
